34039f3919935566c02ee8962df9f7fdc325a226
Saswat Samal <saswat1091@outlook.com>
2024-12-30 21:53:20 +0530

    docs: add landing page and api doc

--- app/main.py
+++ app/main.py
@@ -3,13 +3,38 @@ from . import models, config
 from .database import engine
 from .routers import post, user, auth, vote, comments
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse
 
 
-# uncomment the code below if you plan to use SQLalchemy 
+description = """
+**PostHive** is a dynamic social platform that enables users to share posts, comment, and vote in an interactive and community-driven environment. The name reflects the collaborative and buzzing activity, akin to a hive of ideas and discussions.
+
+Core features include:
+
+- User Management: User authentication (login and registration) with hashed passwords.
+- Posting: Users can create, read, update, and delete text posts.
+- Commenting: Users can comment on posts.
+- Voting: Users can upvote or downvote posts.
+- Sorting: Posts can be retrieved based on popularity or creation time.
+
+"""
+
+# uncomment the code below if you plan to use SQLalchemy
 # instead of Alembic
 # models.Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
+app = FastAPI(
+    title="PostHive API",
+    description=description,
+    version="1.0.0",
+    license_info={
+        "name": "MIT License",
+        "url": "https://github.com/saswat10/postHive-api?tab=MIT-1-ov-file",
+        "identifier": "MIT"
+    },
+    docs_url="/docs",
+    redoc_url=None,
+)
 
 origins = ["*"]
 
@@ -24,7 +49,56 @@ app.add_middleware(
 
 @app.get("/")
 def root():
-    return {"message": "Hello world!!"}
+    content = """
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/simpledotcss/2.3.3/simple.min.css">
+    <title>PostHive - API</title>
+    </head>
+
+    <body>
+    <header>
+        <h1>PostHive</h1>
+    </header>
+    <main>
+
+        <p>PostHive is a dynamic social platform that enables users to share posts, comment, and vote in an interactive and
+        community-driven environment. The name reflects the collaborative and buzzing activity, akin to a hive of ideas
+        and discussions.</p>
+        <article>
+        <h2>Core Features</h2>
+        <ul>
+            <li>User Management: User authentication (login and registration) with hashed passwords.</li>
+            <li>Posting: Users can create, read, update, and delete text posts.</li>
+            <li>Commenting: Users can comment on posts.</li>
+            <li>Voting: Users can upvote or downvote posts.</li>
+            <li>Sorting: Posts can be retrieved based on popularity or creation time.</li>
+        </ul>
+
+        </article>
+
+
+
+        <button onclick="window.location.href='https://posthive-api.onrender.com/docs';">
+        API Documentation
+        </button>
+        <button onclick="window.location.href='https://github.com/saswat10/postHive-api';">
+        GitHub Repository
+        </button>
+        <button onclick="window.location.href='https://github.com/saswat10/postHive-app';">
+        Android App Under Development
+        </button>
+    </body>
+
+    </main>
+
+    </html>
+    """
+    return HTMLResponse(content=content)
 
 
 app.include_router(vote.router)
